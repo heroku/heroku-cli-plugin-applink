efb758ad3d63b1da3ee41e5dde503aed51becda1
Santiago Bosio <santiago.bosio@gmail.com>
2024-08-21 18:20:28 -0300

    Updating the type schemas based on the updated API specification

--- src/lib/integration/types.ts
+++ src/lib/integration/types.ts
@@ -2,18 +2,42 @@
  * An connection between a Heroku app and an integration platform.
  */
 export type Connection = {
-  /** unique identifier of a connection */
-  readonly id: string
-  /** org info */
-  readonly org: {
-    readonly id: string
-    readonly alias: string
-    readonly instance_url: string
+  /** connection ID */
+  readonly id?: string
+  /** Salesforce Org info */
+  readonly salesforce_org: {
+    readonly id?: string | null
+    readonly instance_url?: string | null
+    readonly org_name: string
+    readonly run_as_user?: string | null
   }
-  /** run as user */
-  readonly run_as_user: string
+  /** redirect URI for authentication */
+  readonly redirect_uri?: string | null
   /** connection state */
-  readonly state: 'connecting' | 'connected' | 'connection_failed'
-  /** integration type */
-  readonly type: 'datacloud' | 'salesforce'
+  readonly state:
+    'pending' | 'authenticating' | 'authenticated' | 'authentication_failed' |
+    'connecting' | 'connected' | 'connection_failed' | 'disconnecting' | 'disconnected'
+  /** connection type */
+  readonly 'type': 'DatacloudOrg' | 'SalesforceOrg'
+}
+
+/**
+ * An app import process.
+ */
+export type AppImport = {
+  /** client name */
+  readonly client_name: string
+  /** generate authorization permission set */
+  readonly generate_authorization_permission_set: boolean
+  /** app import ID */
+  readonly id: string
+  /** Salesforce Org info */
+  readonly salesforce_org: {
+    readonly id?: string | null
+    readonly instance_url?: string | null
+    readonly org_name: string
+    readonly run_as_user?: string | null
+  }
+  /** import process state */
+  readonly state: 'pending' | 'importing' | 'imported' | 'import_failed'
 }
