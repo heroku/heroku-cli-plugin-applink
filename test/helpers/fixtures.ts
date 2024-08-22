ee61ce66523cd30e9a884bf7936be04bd6dfb246
Santiago Bosio <santiago.bosio@gmail.com>
2024-08-22 17:10:10 -0300

    Adding command 'salesforce:connect' and tests

--- test/helpers/fixtures.ts
+++ test/helpers/fixtures.ts
@@ -1,4 +1,7 @@
-export const addon = {
+import * as Heroku from '@heroku-cli/schema'
+import * as Integration from '../../src/lib/integration/types'
+
+export const addon: Heroku.AddOn = {
   name: 'heroku-integration-vertical-01234',
   addon_service: {
     id: '01234567-89ab-cdef-0123-456789abcdef',
@@ -6,26 +9,47 @@ export const addon = {
   },
 }
 
-export const connection1 = {
-  id: '01234567-89ab-cdef-0123-456789abcdef',
-  org: {
+export const connection1: Integration.Connection = {
+  salesforce_org: {
     id: '00DSG000007a3FdB96',
-    alias: 'fake-sfdc-org',
     instance_url: 'https://dsg000007a3fdb96.test1.my.pc-rnd.salesforce.com',
+    org_name: 'my-org-1',
+    run_as_user: 'user@example.com',
   },
-  run_as_user: 'user@example.com',
   state: 'connected',
-  type: 'salesforce',
+  type: 'SalesforceOrg',
 }
 
-export const connection2 = {
-  id: '456789ab-cdef-0123-4567-89abcdef0123',
-  org: {
-    id: '00DSG00000B72aF69s',
-    alias: 'another-sfdc-org',
-    instance_url: 'https://dsg00000b72af69s.test1.my.pc-rnd.salesforce.com',
+export const connection2_connecting: Integration.Connection = {
+  salesforce_org: {
+    id: '',
+    instance_url: '',
+    org_name: 'my-org-2',
+    run_as_user: '',
   },
-  run_as_user: 'user2@example.com',
+  redirect_uri: 'https://login.test1.my.pc-rnd.salesforce.com/services/oauth2/authorize',
+  state: 'authenticating',
+  type: 'SalesforceOrg',
+}
+
+export const connection2_connected: Integration.Connection = {
+  salesforce_org: {
+    id: '00DSG000007a3BcA84',
+    instance_url: 'https://dsg000007a3bca84.test1.my.pc-rnd.salesforce.com',
+    org_name: 'my-org-2',
+    run_as_user: 'user@example.com',
+  },
   state: 'connected',
-  type: 'salesforce',
+  type: 'SalesforceOrg',
+}
+
+export const connection2_failed: Integration.Connection = {
+  salesforce_org: {
+    id: '00DSG000007a3BcA84',
+    instance_url: 'https://dsg000007a3bca84.test1.my.pc-rnd.salesforce.com',
+    org_name: 'my-org-2',
+    run_as_user: '',
+  },
+  state: 'connection_failed',
+  type: 'SalesforceOrg',
 }
