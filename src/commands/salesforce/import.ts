1fff2e1eeeca30d0c0e3e6538f8ede5fe6e7799a
Katy Bowman <katy.bowman@salesforce.com>
2024-08-22 16:50:34 -0400

    add boilerplate for command

---
+++ src/commands/salesforce/import.ts
@@ -0,0 +1,16 @@
+import Command from '../../lib/base'
+import {flags} from '@heroku-cli/command'
+import {ux, Args} from '@oclif/core'
+
+
+export default class Import extends Command {
+  static description = ''
+
+  static flags = {}
+
+  static args = {}
+
+  public async run(): Promise<void> {
+    
+  }
+}
