ed846a4552b4cea4bb4f4f65db700c3922aa0651
cwallsfdc <cwall@salesforce.com>
2024-09-06 11:26:50 -0600

    Stand-up until API is available

--- src/commands/datacloud/data-action-target/create.ts
+++ src/commands/datacloud/data-action-target/create.ts
@@ -1,3 +1,4 @@
+import {color} from '@heroku-cli/color'
 import Command from '../../../lib/base'
 import {flags} from '@heroku-cli/command'
 import * as Integration from '../../../lib/integration/types'
@@ -19,9 +20,21 @@ export default class Create extends Command {
 
   public async run(): Promise<void> {
     const {flags, args} = await this.parse(Create)
-    const {app, 'org-name': orgName, type, 'api-name': apiName} = flags
+    const {app, 'org-name': orgName, type} = flags
+    let {'api-name': apiName} = flags
     const {label} = args
 
+    if (!apiName) {
+      // More encoding needed
+      apiName = label.replaceAll(' ', '_')
+    }
+
+    const msg = `Create Data Action Target ${type} '${color.configVar(label)}' for ${color.app(app)} in org ${color.configVar(orgName)}`
+    ux.action.start(msg, 'creating')
+    await ux.wait(2500)
+    ux.action.start(msg, 'done')
+
+    /* WIP Awaiting API updates
     await this.configureIntegrationClient(app)
     await this.integration.post<Integration.Connection>(
       `/datacloud/${orgName}/data_action_targets`,
@@ -33,6 +46,7 @@ export default class Create extends Command {
         },
       }
     )
+    */
 
     ux.action.stop()
   }
