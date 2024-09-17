106cc068bc46149711824fc9ab44c04e66f66450
cwallsfdc <cwall@salesforce.com>
2024-09-17 17:12:21 -0600

    Update command description

--- src/commands/datacloud/data-action-target/create.ts
+++ src/commands/datacloud/data-action-target/create.ts
@@ -7,7 +7,7 @@ import {humanize} from '../../../lib/helpers'
 import heredoc from 'tsheredoc'
 
 export default class Create extends Command {
-  static description = 'create Data Action Target webhook for a Heroku app'
+  static description = 'create Data Action Target for a Heroku app'
 
   static flags = {
     app: flags.app({required: true}),
