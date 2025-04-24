d33df12841306d125e4b9d7e3268d423879a3eca
Katy Bowman <katy.bowman@salesforce.com>
2025-04-24 15:57:16 -0400

    test: add tests for applink:authorizations command

---
+++ test/commands/applink/authorizations/index.test.ts
@@ -0,0 +1,79 @@
+import {expect} from 'chai'
+import nock from 'nock'
+import {stderr, stdout} from 'stdout-stderr'
+import heredoc from 'tsheredoc'
+import {runCommand} from '../../../run-command'
+import Cmd from '../../../../src/commands/applink/authorizations/index'
+import stripAnsi from '../../../helpers/strip-ansi'
+import {
+  addon,
+  authorization1,
+  authorization2,
+} from '../../../helpers/fixtures'
+
+describe('applink:authorizations', function () {
+  let api: nock.Scope
+  let integrationApi: nock.Scope
+  const {env} = process
+
+  beforeEach(function () {
+    process.env = {}
+    api = nock('https://api.heroku.com')
+    integrationApi = nock('https://integration-api.heroku.com')
+  })
+
+  afterEach(function () {
+    process.env = env
+    api.done()
+    integrationApi.done()
+    nock.cleanAll()
+  })
+
+  context('when the --app flag is specified', function () {
+    beforeEach(function () {
+      api.get('/apps/my-app/addons')
+        .reply(200, [addon])
+        .get('/apps/my-app/config-vars')
+        .reply(200, {
+          HEROKU_INTEGRATION_API_URL: 'https://integration-api.heroku.com/addons/01234567-89ab-cdef-0123-456789abcdef',
+        })
+    })
+
+    context('when there are no Heroku AppLink authorizations created on the app', function () {
+      it('displays a notification', async function () {
+        integrationApi
+          .get('/addons/01234567-89ab-cdef-0123-456789abcdef/authorizations')
+          .reply(200, [])
+
+        await runCommand(Cmd, [
+          '--app=my-app',
+        ])
+
+        expect(stripAnsi(stdout.output)).to.equal('There are no Heroku AppLink authorizations for add-on heroku-integration-vertical-01234 on app my-app.\n')
+        expect(stderr.output).to.equal('')
+      })
+    })
+
+    context('when there are Heroku AppLink authorizations returned', function () {
+      it('shows the authorizations', async function () {
+        integrationApi
+          .get('/addons/01234567-89ab-cdef-0123-456789abcdef/authorizations')
+          .reply(200, [authorization1, authorization2])
+
+        await runCommand(Cmd, [
+          '--app=my-app',
+        ])
+
+        expect(stripAnsi(stdout.output)).to.equal(heredoc`
+          === Heroku AppLink authorizations for app my-app
+  
+           Type           Add-On                            Connected Org Developer Name    Status       
+           ────────────── ───────────────────────────────── ───────────── ───────────────── ──────────── 
+           Salesforce Org heroku-integration-vertical-01234 my-org-1      my-developer-name Connected    
+           Salesforce Org heroku-integration-vertical-01234 my-org-2      my-developer-name Disconnected 
+        `)
+        expect(stderr.output).to.equal('')
+      })
+    })
+  })
+})
