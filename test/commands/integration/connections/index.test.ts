b17f0395a9466e23d44803dc116c5aaa69f8ba5c
Zane Whitfield <zwhitfield@salesforce.com>
2025-05-01 11:56:31 -0700

    Update tests

--- test/commands/integration/connections/index.test.ts
+++ test/commands/integration/connections/index.test.ts
@@ -7,11 +7,8 @@ import Cmd from '../../../../src/commands/integration/connections/index'
 import stripAnsi from '../../../helpers/strip-ansi'
 import {
   addon,
-  addon2,
   connection1,
   connection2_connected,
-  connection3,
-  connection4_connected,
 } from '../../../helpers/fixtures'
 
 describe('integration:connections', function () {
@@ -75,73 +72,6 @@ describe('integration:connections', function () {
            ────────────── ──────── ───────── ──────────────── 
            Salesforce Org my-org-1 Connected user@example.com 
            Salesforce Org my-org-2 Connected user@example.com 
-        `)
-        expect(stderr.output).to.equal('')
-      })
-    })
-  })
-
-  context('when the --app flag isn’t specified', function () {
-    context('when there are no Heroku Integration addons', function () {
-      beforeEach(function () {
-        api.get('/addons').reply(200, [])
-      })
-
-      it('displays a notification', async function () {
-        await runCommand(Cmd)
-
-        expect(stripAnsi(stdout.output)).to.equal('No Heroku Integration connections.\n')
-        expect(stderr.output).to.equal('')
-      })
-    })
-
-    context('when there are Heroku Integration addons', function () {
-      beforeEach(function () {
-        api.get('/addons')
-          .reply(200, [addon, addon2])
-          .get('/apps/89abcdef-0123-4567-89ab-cdef01234567/config-vars')
-          .reply(200, {
-            HEROKU_APPLINK_API_URL: 'https://integration-api.heroku.com/addons/01234567-89ab-cdef-0123-456789abcdef',
-            HEROKU_APPLINK_TOKEN: '01234567-89ab-cdef-0123-456789abcdef',
-          })
-          .get('/apps/abcdef01-2345-6789-abcd-ef0123456789/config-vars')
-          .reply(200, {
-            HEROKU_APPLINK_API_URL: 'https://integration-api.heroku.com/addons/6789abcd-ef01-2345-6789-abcdef012345',
-            HEROKU_APPLINK_TOKEN: '01234567-89ab-cdef-0123-456789abcdef',
-          })
-      })
-
-      it('displays a notification when there are no Heroku Integration connections on any app', async function () {
-        integrationApi
-          .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections')
-          .reply(200, [])
-          .get('/addons/6789abcd-ef01-2345-6789-abcdef012345/connections')
-          .reply(200, [])
-
-        await runCommand(Cmd)
-
-        expect(stripAnsi(stdout.output)).to.equal('No Heroku Integration connections.\n')
-        expect(stderr.output).to.equal('')
-      })
-
-      it('shows the connections when there are Heroku Integration connections returned', async function () {
-        integrationApi
-          .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections')
-          .reply(200, [connection1, connection2_connected])
-          .get('/addons/6789abcd-ef01-2345-6789-abcdef012345/connections')
-          .reply(200, [connection3, connection4_connected])
-
-        await runCommand(Cmd)
-
-        expect(stripAnsi(stdout.output)).to.equal(heredoc`
-          === Heroku Integration connections
-
-           App          Type           Org Name Status     Run As User       
-           ──────────── ────────────── ──────── ────────── ───────────────── 
-           my-app       Salesforce Org my-org-1 Connected  user@example.com  
-           my-app       Salesforce Org my-org-2 Connected  user@example.com  
-           my-other-app Salesforce Org my-org-1 Connecting user2@example.com 
-           my-other-app Data Cloud Org my-org-2 Connected  user@example.com  
         `)
         expect(stderr.output).to.equal('')
       })
