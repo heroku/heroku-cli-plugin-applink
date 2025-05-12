3864bbabb448ebc39209a48a65c6d717b79bcd2f
Zane Whitfield <zwhitfield@salesforce.com>
2025-05-12 14:57:07 -0700

    Update tests

--- test/commands/salesforce/publish.test.ts
+++ test/commands/salesforce/publish.test.ts
@@ -7,7 +7,7 @@ import {
   addon,
   legacyAddon,
 } from '../../helpers/fixtures'
-// import stripAnsi from '../../helpers/strip-ansi'
+import stripAnsi from '../../helpers/strip-ansi'
 // import {CLIError} from '@oclif/core/lib/errors'
 
 describe.only('salesforce:publish', function () {
@@ -50,51 +50,10 @@ describe.only('salesforce:publish', function () {
         '--client-name=AccountAPI',
         '--connection-name=myorg',
       ])
+
+      expect(stripAnsi(stderr.output)).to.contain('Publishing my-app to myorg as AccountAPI')
       expect(stdout.output).to.equal('')
     })
-
-    // it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
-    //   // applinkApi
-    //   //   .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-    //   //   .reply(202, appImportPending)
-
-    //   const filePath = `${__dirname}/../../helpers/openapi.json`
-
-    //   await runCommand(Cmd, [
-    //     filePath,
-    //     '--app=my-app',
-    //     '--client-name=AccountAPI',
-    //     '--generate-auth-permission-set',
-    //     '--org-name=myorg',
-    //   ])
-
-    //   expect(stderr.output).to.contain('Publishing my-app to myconnection as myclient')
-    //   expect(stdout.output).to.equal('')
-    // })
-
-    // it('throws an error when the apps info endpoint status returns "import failed"', async function () {
-    // applinkApi
-    //   .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-    //   .reply(202, appImportPending)
-
-    // const filePath = `${__dirname}/../../helpers/openapi.json`
-
-    // try {
-    //   await runCommand(Cmd, [
-    //     filePath,
-    //     '--app=my-app',
-    //     '--client-name=AccountAPI',
-    //     '--generate-auth-permission-set',
-    //     '--org-name=myorg',
-    //   ])
-    // } catch (error: unknown) {
-    //   const {message, oclif} = error as CLIError
-    //   expect(stripAnsi(message)).to.equal('Import Failed')
-    //   expect(oclif.exit).to.equal(1)
-    // }
-
-    // expect(stdout.output).to.equal('')
-    // })
   })
 
   context('when config var is set to the legacy HEROKU_INTEGRATION_API_URL', function () {
@@ -134,26 +93,9 @@ describe.only('salesforce:publish', function () {
         '--client-name=AccountAPI',
         '--connection-name=myorg',
       ])
+
+      expect(stripAnsi(stderr.output)).to.contain('Publishing my-app to myorg as AccountAPI')
       expect(stdout.output).to.equal('')
     })
-
-  // it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
-  //   integrationApi
-  //     .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-  //     .reply(202, appImportPending)
-
-  //   const filePath = `${__dirname}/../../helpers/openapi.json`
-
-  //   await runCommand(Cmd, [
-  //     filePath,
-  //     '--app=my-app',
-  //     '--client-name=AccountAPI',
-  //     '--generate-auth-permission-set',
-  //     '--org-name=myorg',
-  //   ])
-
-  //   expect(stderr.output).to.contain('Imported')
-  //   expect(stdout.output).to.equal('')
   })
-  // })
 })
