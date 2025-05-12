e8f0f20f48d67d782b5cf9bdd2366d6ef07cc05e
Zane Whitfield <zwhitfield@salesforce.com>
2025-05-12 14:38:10 -0700

    Add first successful test

--- test/commands/salesforce/publish.test.ts
+++ test/commands/salesforce/publish.test.ts
@@ -2,18 +2,15 @@ import {expect} from 'chai'
 import nock from 'nock'
 import {stderr, stdout} from 'stdout-stderr'
 import {runCommand} from '../../run-command'
-import Cmd from './publish'
+import Cmd from '../../../src/commands/salesforce/publish'
 import {
   addon,
-  appImportPending,
-  appImportSuccess,
-  appImportFailed,
   legacyAddon,
 } from '../../helpers/fixtures'
-import stripAnsi from '../../helpers/strip-ansi'
-import {CLIError} from '@oclif/core/lib/errors'
+// import stripAnsi from '../../helpers/strip-ansi'
+// import {CLIError} from '@oclif/core/lib/errors'
 
-describe('salesforce:import', function () {
+describe.only('salesforce:publish', function () {
   let api: nock.Scope
   let applinkApi: nock.Scope
   const {env} = process
@@ -39,104 +36,111 @@ describe('salesforce:import', function () {
       nock.cleanAll()
     })
 
-    it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
+    it('successfully publishes an API spec file', async function () {
       applinkApi
-        .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-        .reply(202, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportSuccess)
+        .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/apps')
+        .reply(201, [])
 
       const filePath = `${__dirname}/../../helpers/openapi.json`
 
       await runCommand(Cmd, [
         filePath,
         '--app=my-app',
+        '--addon=heroku-applink-vertical-01234',
         '--client-name=AccountAPI',
-        '--generate-auth-permission-set',
-        '--org-name=myorg',
+        '--connection-name=myorg',
       ])
-
-      expect(stderr.output).to.contain('Imported')
       expect(stdout.output).to.equal('')
     })
 
-    it('throws an error when the apps info endpoint status returns "import failed"', async function () {
-      applinkApi
-        .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-        .reply(202, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportFailed)
+    // it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
+    //   // applinkApi
+    //   //   .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
+    //   //   .reply(202, appImportPending)
 
-      const filePath = `${__dirname}/../../helpers/openapi.json`
+    //   const filePath = `${__dirname}/../../helpers/openapi.json`
 
-      try {
-        await runCommand(Cmd, [
-          filePath,
-          '--app=my-app',
-          '--client-name=AccountAPI',
-          '--generate-auth-permission-set',
-          '--org-name=myorg',
-        ])
-      } catch (error: unknown) {
-        const {message, oclif} = error as CLIError
-        expect(stripAnsi(message)).to.equal('Import Failed')
-        expect(oclif.exit).to.equal(1)
-      }
+    //   await runCommand(Cmd, [
+    //     filePath,
+    //     '--app=my-app',
+    //     '--client-name=AccountAPI',
+    //     '--generate-auth-permission-set',
+    //     '--org-name=myorg',
+    //   ])
 
-      expect(stdout.output).to.equal('')
-    })
+    //   expect(stderr.output).to.contain('Publishing my-app to myconnection as myclient')
+    //   expect(stdout.output).to.equal('')
+    // })
+
+    // it('throws an error when the apps info endpoint status returns "import failed"', async function () {
+    // applinkApi
+    //   .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
+    //   .reply(202, appImportPending)
+
+    // const filePath = `${__dirname}/../../helpers/openapi.json`
+
+    // try {
+    //   await runCommand(Cmd, [
+    //     filePath,
+    //     '--app=my-app',
+    //     '--client-name=AccountAPI',
+    //     '--generate-auth-permission-set',
+    //     '--org-name=myorg',
+    //   ])
+    // } catch (error: unknown) {
+    //   const {message, oclif} = error as CLIError
+    //   expect(stripAnsi(message)).to.equal('Import Failed')
+    //   expect(oclif.exit).to.equal(1)
+    // }
+
+    // expect(stdout.output).to.equal('')
+    // })
   })
 
-  context('when config var is set to the legacy HEROKU_INTEGRATION_API_URL', function () {
-    let integrationApi: nock.Scope
+  // context('when config var is set to the legacy HEROKU_INTEGRATION_API_URL', function () {
+  //   let integrationApi: nock.Scope
 
-    beforeEach(function () {
-      process.env = {}
-      api = nock('https://api.heroku.com')
-        .get('/apps/my-app/addons')
-        .reply(200, [legacyAddon])
-        .get('/apps/my-app/config-vars')
-        .reply(200, {
-          HEROKU_INTEGRATION_API_URL: 'https://integration-api.heroku.com/addons/01234567-89ab-cdef-0123-456789abcdef',
-          HEROKU_INTEGRATION_TOKEN: '01234567-89ab-cdef-0123-456789abcdef',
-        })
-      integrationApi = nock('https://integration-api.heroku.com')
-    })
+  //   beforeEach(function () {
+  //     process.env = {}
+  //     api = nock('https://api.heroku.com')
+  //       .get('/apps/my-app/addons')
+  //       .reply(200, [legacyAddon])
+  //       .get('/apps/my-app/config-vars')
+  //       .reply(200, {
+  //         HEROKU_INTEGRATION_API_URL: 'https://integration-api.heroku.com/addons/01234567-89ab-cdef-0123-456789abcdef',
+  //         HEROKU_INTEGRATION_TOKEN: '01234567-89ab-cdef-0123-456789abcdef',
+  //       })
+  //     integrationApi = nock('https://integration-api.heroku.com')
+  //   })
 
-    afterEach(function () {
-      process.env = env
-      api.done()
-      integrationApi.done()
-      nock.cleanAll()
-    })
+  //   afterEach(function () {
+  //     process.env = env
+  //     api.done()
+  //     integrationApi.done()
+  //     nock.cleanAll()
+  //   })
 
-    it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
-      integrationApi
-        .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
-        .reply(202, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportPending)
-        .get('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports/AccountAPI')
-        .reply(200, appImportSuccess)
+  //   it('successfully publishes the app api-spec to the Salesforce org', async function () {
+  //     expect(true).to.equal(true)
+  //   })
 
-      const filePath = `${__dirname}/../../helpers/openapi.json`
+  // it('waits for the apps info endpoint status to return "imported" before ending the action successfully', async function () {
+  //   integrationApi
+  //     .post('/addons/01234567-89ab-cdef-0123-456789abcdef/connections/salesforce/myorg/app_imports')
+  //     .reply(202, appImportPending)
 
-      await runCommand(Cmd, [
-        filePath,
-        '--app=my-app',
-        '--client-name=AccountAPI',
-        '--generate-auth-permission-set',
-        '--org-name=myorg',
-      ])
+  //   const filePath = `${__dirname}/../../helpers/openapi.json`
 
-      expect(stderr.output).to.contain('Imported')
-      expect(stdout.output).to.equal('')
-    })
-  })
+  //   await runCommand(Cmd, [
+  //     filePath,
+  //     '--app=my-app',
+  //     '--client-name=AccountAPI',
+  //     '--generate-auth-permission-set',
+  //     '--org-name=myorg',
+  //   ])
+
+  //   expect(stderr.output).to.contain('Imported')
+  //   expect(stdout.output).to.equal('')
+  // })
+  // })
 })
