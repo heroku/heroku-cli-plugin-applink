29b3821591bf93a52d6089d1f71954f288132414
Zane Whitfield <zwhitfield@salesforce.com>
2025-05-21 11:39:52 -0700

    Add new error object response part 1

--- src/commands/applink/authorizations/index.ts
+++ src/commands/applink/authorizations/index.ts
@@ -17,12 +17,18 @@ export default class Index extends Command {
   public async run(): Promise<void> {
     const {flags} = await this.parse(Index)
     const {addon, app} = flags
+    let appAuthorizations: AppLink.Authorization[]
 
     await this.configureAppLinkClient(app, addon)
-    const {body: appAuthorizations} = await this.applinkClient.get<AppLink.Authorization[]>(`/addons/${this.addonId}/authorizations`, {
-      headers: {authorization: `Bearer ${this._applinkToken}`},
-      retryAuth: false,
-    })
+    try {
+      ({body: appAuthorizations} = await this.applinkClient.get<AppLink.Authorization[]>(`/addons/${this.addonId}/authorizations`, {
+        headers: {authorization: `Bearer ${this._applinkToken}`},
+        retryAuth: false,
+      }))
+    } catch (error) {
+      const applinkError = error as AppLink.StandardApplinkError
+      throw applinkError
+    }
 
     if (appAuthorizations.length === 0) {
       ux.log(`There are no Heroku AppLink authorizations for add-on ${this._addonName} on app ${color.app(app)}.`)
